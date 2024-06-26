b7df6c5f748947de4afa5ef9a93bc50e1ce8217c
leonid <leonidsleo@yandex.ru>
2024-06-26 20:40:22 +0300

    решение ДЗ к семинару 4

--- hometaskproject/seminar3/models.py
+++ hometaskproject/seminar3/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10)
     quantity = models.DecimalField(max_digits=10, decimal_places=3)
     data_add = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='media/')
 
 
     def __str__(self):
